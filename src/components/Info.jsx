14864de48aaa906c2ed9279354b5b0f2c8f6f3c5
Alexey <obellexa3@mail.ru>
2024-12-08 00:33:24 +0300

    changes Info

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -3,7 +3,14 @@ import React, { useState } from "react";
 const Info = () => {
   return (
     <div className="info">
-      <h1 className="info-title">Information about the Website</h1>
+      <h1 className="info-title">Information</h1>
+      <p className="info-text">
+        On our website you can find many useful tools such as a calculator,
+        timer and others.
+      </p>
+      <p className="info-text">
+        There is support on our website, links to our social networks.
+      </p>
       <p className="info-text"></p>
     </div>
   );
